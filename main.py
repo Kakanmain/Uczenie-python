42ac0daa36fd381b63d2d78f770be2aa123db9b1
Kakanmain <144654657+Kakanmain@users.noreply.github.com>
2023-11-28 11:04:30 +0100

    Add files via upload

--- main.py
+++ main.py
@@ -13,6 +13,7 @@ imie="Bartosz"
 wiek=16 #Określenie zmiennej 'wiek' na 16
 print(imie) #Wydala kod 'imie'
 print(wiek)
+print("")
 
 #określanie typu zmiennej
 print("Określanie typie zmiennej")
@@ -25,22 +26,26 @@ print(wiek_z,"float")
 print(type(wiek_x)) #Mówi nam o typie zmiennej
 print(type(wiek_y))
 print(type(wiek_z))
+print("")
 
 #'' i " " niczym sie nie różni
 imie="Bartosz"
 print(imie)
 imie='Bartosz'
 print(imie)
+print("")
 
 #duże znaki zmiennych różnią się
 Imie="Kordian"
 print(Imie)
 print(imie)
+print("")
 
 #Zmienne nie mogą się zaczynać liczbą i nie mogą mieć w sobie spacji innych znaków specjalnych, oprócz _
 _moje_2imie="Łukasz" #git
 print(_moje_2imie)
 #2moje - imie="Łukasz" #nie git
+print("")
 
 #Wpisywanie wiele zmiennych
 #Wiele zmiennych do wielu wartości
@@ -49,6 +54,7 @@ x, y, z="procesor", "karta graficzna", "RAM"
 print(x)
 print(y)
 print(z)
+print("")
 
 #Wiele zmiennych do jednej wartości
 print("Wiele zmiennych do jednej wartości")
@@ -56,6 +62,7 @@ x=y=z="zasilacz"
 print(x)
 print(y)
 print(z)
+print("")
 
 #Odpakowywanie kolekcji
 print("Odpakowywanie kolekcji")
@@ -66,6 +73,7 @@ print(v)
 print(x)
 print(y)
 print(z)
+print("")
 
 #Zmienne wyjściowe
 print(imie)
@@ -74,6 +82,7 @@ print(imie, wiek) #string, int --git
 print(imie+Imie)
 rok=2022
 print(rok+wiek) #int + int --git
+print("")
 
 #Globalne zmienne
 miasto="Siemce" #zmienna globalna
@@ -90,6 +99,7 @@ def test3():
   miasto="Będzin" #zmienna w funkcji jest globalna
 test3()
 print("Moje miasto to "+miasto) #print jest poza funkcją, lecz bierze będzin bo jest zmienną globalną
+print("")
 
 #TYPY ZMIENNYCH
 """
@@ -106,12 +116,14 @@ None Type: 	NoneType
 #losowa liczba
 import random
 print(random.randrange(1, 10))
+print("")
 
 #STRING - ZWYKŁY TEKST
 print("string")
 x="Bartosz" #Bez określenia zmiennej
 x=str("Bartosz") #Z okresleniem zmiennej.
 print(x)
+print("")
 
 #wiele linijek
 y="""Lorem ipsum dolor sit amet,
@@ -119,18 +131,22 @@ consectetur adipiscing elit,
 sed do eiusmod tempor incididunt
 ut labore et dolore magna aliqua.""" #By dać string w wielu linijkach trzeba dać """ """ lub ''' '''
 print(y)
+print("")
 
 #Wyciągniecie znaku
 x="XD"
 print(x[0]) #0 to pierwszy znak
+print("")
 
 #pętla for w string'u
 for x in "Bartosz":
   print(x)
+  print("")
 
 #długość string'u
 x="Bartosz"
 print(len(x))
+print("")
 
 #sprawdź czy w string'u
 z="O jasny ****, toż to laseraptor. Myślałem, że wygineły tysiące lat temu"
@@ -143,6 +159,7 @@ z="O jasny ****, toż to laseraptor. Myślałem, że wygineły tysiące lat temu
 print("żyją" not in z)
 if "żyją" not in z:
   print("Jak widać nie żyją")
+  print("")
 
 #slicing string
 b="Witaj świecie!"
@@ -150,6 +167,7 @@ print(b[2:8])
 print(b[:5])
 print(b[2:])
 print(b[-5:-2])
+print("")
 
 #Modify strings
 q=" Witaj, świecie! "
@@ -158,12 +176,14 @@ print(q.lower()) #caps lock off
 print(q.strip()) #usuwa zbędne spacje
 print(q.replace("W", "P")) #Zamienia znaki
 print(q.split(",")) #Rozdziela słowa oddzielone w tym przypadku przecinek
+print("")
 
 #String Cocncatenation - Łączenie ciągów
 a="Witaj"
 b="świecie"
 c= a+" "+b
 print(c)
+print("")
 
 #String format - jak wiadomo nie można łączyc inta z stringiem, przez co można wykorzystać 'format'
 wiek=16
@@ -176,6 +196,7 @@ moje_zamowienie="Poprosze {} kawałki rzeczy o numerze {} za {}"
 print(moje_zamowienie.format(ilosc, nr_rzeczy, cena))
 moje_2_zamowienie="Chciałbym zapłacić {2} za {0} kawałków rzeczy o numerze {1}" #wstawianie w nawiasy kolejności
 print(moje_2_zamowienie.format(ilosc, nr_rzeczy, cena))
+print("")
 
 #Escape characters
 '''nie możemy wpisać np. print("Jesteśmy "najlepsi"") więc to nam to umożliwia'''
@@ -207,6 +228,7 @@ txt = "\110\145\154\154\157"
 print(txt)
 txt = "\x48\x65\x6c\x6c\x6f"
 print(txt)
+print("")
 
 #string methods - https://www.w3schools.com/python/python_strings_methods.asp
 
@@ -222,6 +244,7 @@ w=int("5")
 print(y)
 print(z)
 print(w)
+print("")
 
 #float - liczba wymierna
 print("float")
@@ -234,6 +257,7 @@ print(y)
 print(z)
 v=35e3 #e lub E. float może mieć e by zaznaczyć moc 10
 print(v)
+print("")
 
 #Matematyka
 print("Matematyka")
@@ -248,20 +272,21 @@ print("Dzielenie:", x/z) #Dzielenie
 print("Modulus:", x%y) #Modulus - Reszta z dzielenia
 print("Potegowanie:", x**y) #Potęgowanie
 print("Dzielenie zaokrąglone w dół:", x//z) #Zaokrągla dzielenie w dół, 35/6 = 5.8(3), leczy wynik będzie wynosił 5
+print("")
 
 x=35
 print("Początkowa liczba:", x)
 x+=5
-print("Liczba po dodaniu 5 to:", x)
+print("Liczba 35 po dodaniu 5 to:", x)
 x-=10
-print("Liczba po odjęciu to:", x)
+print("Liczba 40 po odjęciu 10 to:", x)
 x&=13 #AND, aktualnie x=30 czyli binarnie to 11110, a 13 binarnie to 01101, porównując bity te same to 01100, czyli 12
-print(x)
-x|=3
-print(x)
-x^=3
-print(x)
-x>>=1
-print(x)
-x<<=1
-print(x)
+print("Liczba 30 po AND z 13 to:",x)
+x|=5 #OR, aktualnie x=12 binarnie to 1100, a 5 binarnie to 0101, zamieniając 1 za 0 daje nam 1101, czyli 13
+print("Liczba 12 po OR z 5 to:",x)
+x^=3 #XOR, aktualnie x=13 binarnie to 1101, a 3 to 0011, or lecz gdy 1 i 1 się spotkają dają 0, czyli wynik to 1110 czyli 14
+print("Liczba 13 po XOR z 3 to:",x)
+x>>=2 #przesuwa bity w prawo o 2/usuwa dwa ostatnie bity z liczby x=14 binarnie 1110 czy;i wynik to binarnie 0011, czyli 3
+print("Liczba 14 po przesunięciu w prawo o 2 to:",x)
+x<<=5 #przesuwa bity w lewo o 5/dodaje pieć 0 bitów na koniec x=3 binarnie 0011 co daje nam 001100000, czyli 96 (32+64)
+print("Liczba 3 po przesunięciu w lewo o 5 to:",x)
